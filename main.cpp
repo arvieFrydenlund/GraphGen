9cdfbaedd6d12bfc3306d49e54c8aee2b3925ea8
arvie <arvie@cs.toronto.edu>
2025-05-03 20:28:47 -0400

    Doc strings

--- main.cpp
+++ main.cpp
@@ -77,36 +77,59 @@ void test_pybind(string graph_type = "erdos_renyi",
   const int num_nodes = 15, const int batch_size = 7,
   const bool is_casual = true, const bool shuffle_edges = false,
   const bool shuffle_nodes = false, const int min_vocab = 0, int max_vocab = -1,
+  const bool concat_edges = true, const bool query_at_end = true,
   const bool is_flat_model = true, const bool for_plotting = false,
   const int max_edges = 512) {
 
     py::dict d;
     if ( graph_type == "erdos_renyi" ) {
         cout << "Erdos Renyi Graph Test: " << endl;
-        d = erdos_renyi(num_nodes, -1.0, 75, 125, 10, 3, -1, 2, true, is_casual, true, true, 3, num_nodes + 5);
+        d = erdos_renyi(num_nodes, -1.0, 75, 125,
+            10, 3,
+            -1, 2, true,
+            is_casual, shuffle_edges, shuffle_nodes, min_vocab, max_vocab);
+
     } else  if ( graph_type == "erdos_renyi_n" ) {
+
         cout << "Erdos Renyi Graph Test: " << endl;
-        d = erdos_renyi_n(num_nodes, num_nodes + 5, -1.0, 75, 125,  10, 3, true,
-                          is_casual, true, true, 2,  num_nodes + 10, batch_size, max_edges);
+        d = erdos_renyi_n(num_nodes, num_nodes + 5, -1.0, 75, 125,
+            10, 3, true,
+            -1, 2, false, false,
+            is_casual, shuffle_edges, shuffle_nodes, min_vocab, max_vocab, batch_size, max_edges,
+            concat_edges, query_at_end, 0, is_flat_model, for_plotting
+            );
+
     } else if ( graph_type == "euclidian" ) {
+
         cout << "Euclidian Graph Test: " << endl;
         d = euclidian(num_nodes, 2, -1.0, 75, 125, 10, 3, -1, 2, true, is_casual, false, false);
+
     } else  if ( graph_type == "euclidian_n" ) {
+
         cout << "Euclidian Graph Test: " << endl;
         d = euclidian_n(num_nodes, num_nodes + 5, 2, -1.0, 75, 125,  10, 3, true,
             is_casual, true, true, 2,  num_nodes + 10, batch_size, max_edges);
-    } else if ( graph_type == "path_star" ) {  // no need to test this at scale
+
+    } else if ( graph_type == "path_star" ) {
+
         cout << "Path Star Graph Test: " << endl;
         d = path_star(3, 3, 5, 5, -1, 2, true, false, false);
+
     } else if ( graph_type == "path_star_n" ) {
+
         cout << "Path Star Graph Test: " << endl;
         d = path_star_n(2, 3, 5, 7, true, is_casual, true, true, 3, num_nodes + 10, batch_size);
+
     } else if ( graph_type == "balanced" ) {
+
         cout << "Balanced Graph Test: " << endl;
         d = balanced(num_nodes, 7, 5, 4, -1, -1, 2, true, is_casual, false, false, 0, -1);
+
     } else if ( graph_type == "balanced_n" ) {
+
         cout << "Balanced Graph Test: " << endl;
         d = balanced_n(num_nodes, num_nodes + 5,  3, 8, 4, 4, -1, true, is_casual, true, true, 3, num_nodes + 15, batch_size);
+
     } else {
         cout << "Unknown graph type: " << graph_type << endl;
         return;
@@ -151,14 +174,25 @@ int main(){
     // cout << "Dict: " << &d << endl;
 
     cout << "Seed: " << get_seed() << endl;
-    set_seed(3344391079);
-    test_pybind("erdos_renyi");
+
+    const int num_nodes = 15;
+    const int batch_size = 7;
+    const bool is_casual = true;
+    const bool shuffle_edges = false;
+    const bool shuffle_nodes = false;
+    const int min_vocab = 0;
+    int max_vocab = -1;
+    const bool is_flat_model = true;
+    const bool for_plotting = false;
+
+
+    test_pybind("erdos_renyi", num_nodes, batch_size, is_casual, shuffle_edges, shuffle_nodes, min_vocab, max_vocab, is_flat_model, for_plotting);
     // test_pybind("euclidian");
     // test_pybind("path_star");
     // test_pybind("balanced", 25);
 
     auto t1 = time_before();
-    // test_pybind("erdos_renyi_n", 50, 256, false);
+    // test_pybind("erdos_renyi_n", num_nodes, batch_size, is_casual, shuffle_edges, shuffle_nodes, min_vocab, max_vocab, is_flat_model, for_plotting);
     // test_pybind("erdos_renyi_n", 150, 256);
     // test_pybind("euclidian_n", 50, 256, false);
     // test_pybind("path_star_n", 50, 256, false);
